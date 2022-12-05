d7c7971171d7225ffbd3441dcff226c6b1f07328
joeyclapton <joeyclapton42@gmail.com>
2022-12-04 22:53:58 -0300

    feat: add day 05

---
+++ day-05/index.js
@@ -0,0 +1,19 @@
+import products from "./data.js";
+
+function getNewItems(oldItem) {
+    const { item, price } = oldItem;
+    
+    return {
+        item, 
+        price
+    };
+}
+
+function getSaleItems(data){
+    const sweetItems = data.filter((item) => item.type === 'sweet');
+    const newItems = sweetItems.map(getNewItems);
+    
+    return newItems;
+};
+
+console.log(getSaleItems(products))
